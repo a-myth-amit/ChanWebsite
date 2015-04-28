2d8963bf5a2701c984c9a3243727cf2a5ea6aff7
a-myth-amit <a-myth-amit@users.noreply.github.com>
2015-04-28 06:46:14 -0500

    Update app.js

--- app.js
+++ app.js
@@ -13,11 +13,13 @@ console.log("The script executing is present here " + __dirname);
 console.log("Setting view to " + app.get('views'));
 app.set('view engine', 'jade');
 console.log("Setting view to " + app.get('view engine'));
+app.set('port', process.env.PORT || 3000);
 
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
+  console.log('Express server listening on port ' + server.address().port);
   res.render('index',
   { title : 'Jackie s website' })
 });
-app.listen(3000);
+app.listen(8080);
